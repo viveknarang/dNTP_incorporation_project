32cba10c5d4cd53bdbc4408b775c051181594757
Vivek Narang <vivek.narang10@gmail.com>
2018-12-22 19:53:48 -0800

    plotting x label spacing fix ...

--- constants.py
+++ constants.py
@@ -7,5 +7,8 @@ FASTA_FILE = "sequence.fasta"
 PLOTTER_X_LABEL = "Flow Order"
 PLOTTER_Y_LABEL = "Number of Incorporation"
 PLOTTER_TITLE = "dNTP Plot for flow order: "
+PLOTTER_X_LABEL_SPACING_FACTOR = 33.3
 
 FASTA_SEQUENCE_KEY = "AE017221.1_Thermus_thermophilus_HB27_complete_genome"
+
+
--- plotter.py
+++ plotter.py
@@ -1,6 +1,8 @@
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
 from constants import *
 
 
@@ -8,8 +10,10 @@ def plot(flow_order, no_of_incorporation, flow_order_key):
     logging.info("plot: commencing the plotting ...")
     plt.rcdefaults()
     y_pos = np.arange(len(flow_order))
-    plt.bar(y_pos, no_of_incorporation, align='center', alpha=0.5)
+    plt.bar(y_pos, no_of_incorporation, align='center', alpha=0.9)
     plt.xticks(y_pos, flow_order)
+    plt.xticks(y_pos[::math.ceil(len(flow_order)/PLOTTER_X_LABEL_SPACING_FACTOR)])
+
     plt.title(PLOTTER_TITLE + flow_order_key)
     plt.xlabel(PLOTTER_X_LABEL)
     plt.ylabel(PLOTTER_Y_LABEL)
